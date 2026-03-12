e4ebe6fa66dd0aa5739cb7312a7e64744c45c452
Bram Adams <3282661+bramses@users.noreply.github.com>
2026-03-11 20:20:47 -0400

    Add dynamic rendering and revalidation settings to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { unstable_noStore as noStore } from "next/cache";
+
 import { ReadingTimeline } from "@/components/reading-timeline";
 import { getReadingDataset } from "@/lib/reading-data";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function Home() {
+  noStore();
   const data = await getReadingDataset();
 
   return <ReadingTimeline data={data} />;
